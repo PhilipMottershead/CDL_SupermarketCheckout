a110a498595b4d8efc9c3ba113ae406d95664f4b
Philip Mottershead <philipmottershead@gmail.com>
2021-07-09 16:15:21 +0100

    Created initial Spring Project

---
+++ src/main/java/com/cdl/supermarket/checkout/SupermarketCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.cdl.supermarket.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketCheckoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketCheckoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdl/supermarket/checkout/SupermarketCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdl.supermarket.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketCheckoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
